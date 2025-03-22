61605622a16188e88700b5041d0a54b9de9e21e1
Vishal chavan <110799413+vishalmc18@users.noreply.github.com>
2025-03-22 18:39:12 +0530

    Update to save locally

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -77,7 +77,14 @@ export class TaskService {
       dueDate: '2025-02-28',
     },
   ];
+
   
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   getUserTasks(userId: string) {
     console.log('getUserTasks');
@@ -92,9 +99,15 @@ export class TaskService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+  
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
